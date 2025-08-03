aab7b2afbffec860492da2b7b84b6bef21b07222
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-08-03 08:09:10 +0800

    feat: dash shortcuts

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import { Button } from '@/components/ui/button';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -17,6 +18,12 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
+            shortcuts pud ni diri
+            <div class="flex gap-2">
+                <Link :href="route('borrowings.create')">
+                    <Button variant="secondary">Borrow Book</Button>
+                </Link>
+            </div>
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <h1 class="text-8xl">FUNCTIONAL lang ha</h1>
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
