d8b8d2c8d6a3c37230fa2f58c28c6b44723690a8
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-08-08 09:11:02 +0800

    feat: sidebar with hover

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -57,7 +57,7 @@ const footerNavItems: NavItem[] = [
         <SidebarHeader>
             <SidebarMenu>
                 <SidebarMenuItem>
-                    <SidebarMenuButton class="bg-background text-accent-foreground" size="lg" as-child>
+                    <SidebarMenuButton class="bg-background hover:bg-muted text-accent-foreground" size="lg" as-child>
                         <Link :href="route('dashboard')">
                             <AppLogo />
                         </Link>
