222a7461376a5b6cb7c28a38f4015621c42b805b
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-08-13 16:20:15 +0800

    feat: users staff admins, added needed inputs

--- database/migrations/2025_07_31_065935_create_admins_table.php
+++ database/migrations/2025_07_31_065935_create_admins_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('role_title');
-            $table->json('admin_permissions')->nullable();
             $table->timestamps();
         });
     }
--- resources/js/pages/admins/Create.vue
+++ resources/js/pages/admins/Create.vue
@@ -4,6 +4,7 @@ import { type BreadcrumbItem } from '@/types';
 import { Head, useForm } from '@inertiajs/vue3';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import InputError from '@/components/InputError.vue';
 import { Button } from '@/components/ui/button';
 import { LoaderCircle } from 'lucide-vue-next';
@@ -25,8 +26,13 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 const form = useForm({
+    library_id: '',
     first_name: '',
+    middle_initial: '',
     last_name: '',
+    sex: '',
+    contact_number: '',
+    role_title: '',
     email: '',
     password: '',
     password_confirmation: '',
@@ -48,21 +54,60 @@ const submit = () => {
             <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
                 <form @submit.prevent="submit" class="flex flex-col gap-6">
                     <div class="grid grid-cols-4 gap-6">
+
+                        <div class="grid gap-2">
+                            <Label for="library_id">Library ID</Label>
+                            <Input id="library_id" type="text" required :tabindex="1" v-model="form.library_id" placeholder="Library ID" />
+                            <InputError :message="form.errors.library_id" />
+                        </div>
+
                         <div class="grid gap-2">
                             <Label for="first_name">First name</Label>
-                            <Input id="first_name" type="text" required autofocus :tabindex="1" autocomplete="name" v-model="form.first_name" placeholder="First name" />
+                            <Input id="first_name" type="text" required autofocus :tabindex="2" autocomplete="name" v-model="form.first_name" placeholder="First name" />
                             <InputError :message="form.errors.first_name" />
                         </div>
 
                         <div class="grid gap-2">
+                            <Label for="middle_initial">Middle Initial</Label>
+                            <Input id="middle_initial" type="text" :tabindex="3" v-model="form.middle_initial" placeholder="Middle Initial" maxlength="1" />
+                            <InputError :message="form.errors.middle_initial" />
+                        </div>
+
+                        <div class="grid gap-2">
                             <Label for="last_name">Last name</Label>
-                            <Input id="last_name" type="text" required autofocus :tabindex="1" autocomplete="name" v-model="form.last_name" placeholder="Last name" />
+                            <Input id="last_name" type="text" required :tabindex="4" autocomplete="name" v-model="form.last_name" placeholder="Last name" />
                             <InputError :message="form.errors.last_name" />
                         </div>
 
                         <div class="grid gap-2">
+                            <Label for="sex">Sex</Label>
+                            <Select v-model="form.sex" required>
+                                <SelectTrigger id="sex" :tabindex="5">
+                                    <SelectValue placeholder="Select sex" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="male">Male</SelectItem>
+                                    <SelectItem value="female">Female</SelectItem>
+                                </SelectContent>
+                            </Select>
+                            <InputError :message="form.errors.sex" />
+                        </div>
+
+                        <div class="grid gap-2">
+                            <Label for="contact_number">Contact Number</Label>
+                            <Input id="contact_number" type="tel" required :tabindex="6" v-model="form.contact_number" placeholder="Contact Number" />
+                            <InputError :message="form.errors.contact_number" />
+                        </div>
+
+                        <div class="grid gap-2">
+                            <Label for="role_title">Role Title</Label>
+                            <Input id="role_title" type="text" required :tabindex="7" v-model="form.role_title" placeholder="Role Title" />
+                            <InputError :message="form.errors.role_title" />
+                        </div>
+
+                        <div class="grid gap-2">
                             <Label for="email">Email address</Label>
-                            <Input id="email" type="email" required :tabindex="2" autocomplete="email" v-model="form.email" placeholder="email@example.com" />
+                            <Input id="email" type="email" required :tabindex="8" autocomplete="email" v-model="form.email" placeholder="email@example.com" />
                             <InputError :message="form.errors.email" />
                         </div>
 
@@ -72,7 +117,7 @@ const submit = () => {
                                 id="password"
                                 type="password"
                                 required
-                                :tabindex="3"
+                                :tabindex="9"
                                 autocomplete="new-password"
                                 v-model="form.password"
                                 placeholder="Password"
@@ -86,7 +131,7 @@ const submit = () => {
                                 id="password_confirmation"
                                 type="password"
                                 required
-                                :tabindex="4"
+                                :tabindex="10"
                                 autocomplete="new-password"
                                 v-model="form.password_confirmation"
                                 placeholder="Confirm password"
@@ -94,7 +139,7 @@ const submit = () => {
                             <InputError :message="form.errors.password_confirmation" />
                         </div>
 
-                        <Button type="submit" class="mt-2 w-full" tabindex="5" :disabled="form.processing">
+                        <Button type="submit" class="mt-2 w-full" tabindex="11" :disabled="form.processing">
                             <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
                             Create account
                         </Button>
