6e885d6d0747dd204c6c5cd25150901f783b1d6d
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-08-15 00:40:50 +0800

    feat: books create, authors nice

--- resources/js/components/AuthorsTagsInput.vue
+++ resources/js/components/AuthorsTagsInput.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
-import { ref } from "vue"
 import { TagsInput, TagsInputInput, TagsInputItem, TagsInputItemDelete, TagsInputItemText } from "@/components/ui/tags-input"
+import { ref, watch } from "vue"
 
-const modelValue = ref([])
+// Props and emits
+const props = defineProps<{
+    modelValue: string[]
+}>()
+const emit = defineEmits<{
+    (e: "update:modelValue", value: string[]): void
+}>()
+
+// Internal state synced with parent
+const localValue = ref([...props.modelValue])
+
+// Watch for parent updates
+watch(() => props.modelValue, (newVal) => {
+    localValue.value = [...newVal]
+})
+
+// Watch for local changes to emit back to parent
+watch(localValue, (newVal) => {
+    emit("update:modelValue", newVal)
+}, { deep: true })
 </script>
 
 <template>
-    <TagsInput v-model="modelValue">
-        <TagsInputItem v-for="item in modelValue" :key="item" :value="item">
+    <TagsInput v-model="localValue">
+        <TagsInputItem v-for="item in localValue" :key="item" :value="item">
             <TagsInputItemText />
             <TagsInputItemDelete />
         </TagsInputItem>
