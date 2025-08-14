5d8d2bd19dcc50587a7dfa923a87374669b5b449
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-08-15 01:25:26 +0800

    feat: books create, seeder apas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AcademicSeeder::class);
         $this->call(DdcClassificationSeeder::class);
         $this->call(LcClassificationSeeder::class);
-        $this->call(PhysicalLocation::class);
+        $this->call(PhysicalLocationSeeder::class);
     }
 }
