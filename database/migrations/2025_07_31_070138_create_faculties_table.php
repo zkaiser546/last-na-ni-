bd9ddf50c4edae0853d119ad0d40bae1ffe79503
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-08-14 15:02:28 +0800

    feat: faculties create, offices prep

--- database/migrations/2025_07_31_070138_create_faculties_table.php
+++ database/migrations/2025_07_31_070138_create_faculties_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('faculties', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignIdFor('office_id')->nullable()
+            $table->foreignId('office_id')->nullable()
                 ->constrained()->onDelete('set null');
             $table->string('title'); // e.g., Professor, Associate Professor
             $table->timestamps();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AcademicPeriodSeeder::class);
         $this->call(StatusSeeder::class);
         $this->call(BorrowingPolicySeeder::class);
+        $this->call(OfficeSeeder::class);
     }
 }
