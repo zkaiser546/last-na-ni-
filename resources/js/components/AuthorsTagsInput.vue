b34fdc3a57ba1c84fb5ac317d924a9439fd00634
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-08-15 00:31:15 +0800

    feat: books create, authors better huhu

---
+++ resources/js/components/AuthorsTagsInput.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { TagsInput, TagsInputInput, TagsInputItem, TagsInputItemDelete, TagsInputItemText } from "@/components/ui/tags-input"
+
+const modelValue = ref([])
+</script>
+
+<template>
+    <TagsInput v-model="modelValue">
+        <TagsInputItem v-for="item in modelValue" :key="item" :value="item">
+            <TagsInputItemText />
+            <TagsInputItemDelete />
+        </TagsInputItem>
+
+        <TagsInputInput placeholder="Authors..." />
+    </TagsInput>
+</template>
