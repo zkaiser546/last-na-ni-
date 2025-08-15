ee405b4e765ae7d84a63baf58363d9734c8e30a9
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-08-15 08:35:30 +0800

    feat: thesis view

--- app/Http/Controllers/ThesisController.php
+++ app/Http/Controllers/ThesisController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Thesis;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ThesisController extends Controller
 {
@@ -12,7 +13,7 @@ class ThesisController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('thesis/Index');
     }
 
     /**
